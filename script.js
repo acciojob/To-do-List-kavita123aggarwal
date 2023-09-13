4d03837a2e3df06216d72770c83a61ef6fd18bb3
kavita123aggarwal <118376629+kavita123aggarwal@users.noreply.github.com>
2023-09-14 00:45:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+const inputBox = document.getElementById("newTodoInput");
+const list = document.getElementById("todoList");
 
+function addTask(){
+	if(inputBox.value ==''){
+		alert("you must write something!");
+	}
+	else{
+		let li= document.createElement("li");
+		li.innerHTML = inputBox.value;
+		list.appendChild(li);
+	}
+	inputBox.value = "";
+}
